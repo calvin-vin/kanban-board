1a798e39ffa0bfd45d8d3357955309d83319e5a2
calvin-vin <calvinsan123@gmail.com>
2022-08-30 23:58:56 +0700

    add font and color styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-main": "#01959F",
+        "primary-surface": "#F7FEFF",
+        "secondary-main": "#FA9810",
+        "secondary-border": "#FEEABC",
+        "secondary-surface": "#FFFCF5",
+        "danger-main": "#E11428",
+        "danger-border": "#F5B1B7",
+        "danger-surface": "#FFFAFA",
+        "success-main": "#43936C",
+        "success-border": "#B8DBCA",
+        "success-surface": "#F8FBF9",
+        "neutral-10": "#FFFFFF",
+        "neutral-30": "#EDEDED",
+        "neutral-40": "#E0E0E0",
+        "neutral-70": "#757575",
+        "neutral-90": "#404040",
+        "neutral-100": "#1D1F20",
+      },
+    },
   },
   plugins: [],
 };
